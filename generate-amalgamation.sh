57f65d22025ec95344df2cced3dac3b68cfe4296
John-Tuesday <10697715+John-Tuesday@users.noreply.github.com>
2025-03-18 05:04:26 -0500

    Default amalgamation builder for current release
    sqlite version: 3.49.1
    
    Generate default amalgamation
    
    Add CMake integration

---
+++ generate-amalgamation.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+
+readonly build_rel_dir="build"
+readonly amalgamation_dest_rel_dir="sqlite3-amalgamation"
+
+function configure() {
+  (cd ${build_rel_dir} && ../sqlite/configure)
+}
+
+function make-amalgamation() {
+  (cd ${build_rel_dir} && make sqlite3.c)
+}
+
+function create-payload() {
+  mkdir --verbose --parents ${amalgamation_dest_rel_dir}
+  cp "--target-directory=${amalgamation_dest_rel_dir}" ${build_rel_dir}/sqlite3.c ${build_rel_dir}/sqlite3.h
+}
+
+function clean() {
+  rm -r ${build_rel_dir}/*
+  rm -r ${amalgamation_dest_rel_dir}/*
+}
+
+mkdir --verbose --parents ${build_rel_dir}
+configure 
+make-amalgamation 
+create-payload
